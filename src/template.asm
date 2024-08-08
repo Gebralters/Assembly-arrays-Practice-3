7e1ec8197f8c97ad3e9f48df7825ae9982cc3550
Gebralters <lethebageberalters@gmail.com>
2024-08-08 08:41:14 +0200

    global variables declaration

--- src/template.asm
+++ src/template.asm
@@ -4,25 +4,33 @@
 .MODEL FLAT ; Flat memory model
 .STACK 4096 ; 4096 bytes
 
+INCLUDE io.inc
+
 ; Exit function
 ExitProcess PROTO NEAR32 stdcall, dwExitCode:DWORD
 
 ; The data section stores all global variables
 .DATA
+;<=========================gLOBAL VARIABLES================================>
+kernel DWORD ?
+bias DWORD ?
+array DWORD 4 DUP (?)
 
-NUM1 DWORD 20
-NUM2 DWORD 20
-ANS DWORD ?
+strbias   BYTE "Enter the bias:",0
+strkernel BYTE "Enter the kernel:",0
+strprompt BYTE "Enter the number in the array on index ",0
+strcolon  BYTE ":",0
+strNL     BYTE 10,0
+
 
 
 .CODE
 _start:
-	MOV  EAX , NUM1
-	MOV EBX, NUM2
-	MUL EBX
-	MOV EBX , 10
-	DIV EBX
-	MOV ANS, EAX
+
+
+
+
+exitprogram:
 	INVOKE ExitProcess, 0
 Public _start
 END
